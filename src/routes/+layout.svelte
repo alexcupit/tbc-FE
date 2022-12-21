5bcaad937fb45b80d7fe61a0004ed8f9be641270
alexcupit <alexcupit@outlook.com>
2022-12-21 16:46:30 +0000

    layout file containing responsive Nav component

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Nav from './Nav.svelte';
+</script>
+
+<div class="app">
+	<Nav />
+</div>
+
+<style></style>
---
+++ src/routes/Nav.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import Hamburger from 'hamburger-menu-svelte';
+	const menu_list = [{ name: 'Leaderboard', url: '/' }];
+	let innerWidth: number;
+</script>
+
+<svelte:window bind:innerWidth />
+
+<header class="header">
+	<p class="header__logo">Logo</p>
+
+	<nav class="navbar">
+		{#if innerWidth <= 425}
+			<Hamburger {menu_list} />
+		{:else}
+			<a href="/" class="navbar__link">Leaderboard</a>
+			<a href="/" class="navbar__link">How to play</a>
+			<a href="/" class="navbar__link">Log in</a>
+		{/if}
+	</nav>
+</header>
+
+<style>
+	.header {
+		display: flex;
+		justify-content: space-between;
+		width: 100vw;
+		border: blue 3px solid;
+	}
+
+	.header__logo {
+		border: 2px green solid;
+		height: 100%;
+	}
+
+	.navbar {
+		border: 2px solid black;
+		display: flex;
+		align-items: center;
+	}
+
+	.navbar__link {
+		margin: 0 5px;
+		border: black 1px solid;
+	}
+</style>
