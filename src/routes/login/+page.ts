43b58acdbf32df57301e07216101b1c4ebfdacee
zakaria552 <zakaria.farah55@outlook.com>
2022-12-30 10:53:30 +0000

    added routes for login, profile

---
+++ src/routes/login/+page.ts
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+// export const mongoDbcreateUser = async (body: {}) => {
+// 	const createdUser = await axios.post('https://wild-blue-rooster-belt.cyclic.app/api/users', body).catch((err) => {
+//         console.log(err)
+//     });
+//     console.log(createdUser)
+// }
---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import userStore from "../../stores/userStore"
+    let user;
+    userStore.subscribe((data) => user = data)
+</script>
+
+<h3>Profile info</h3>
+<p>Username: {user.username}</p>
+<p>Last played: {user.dateLastPlayed}</p>
+<p>Current streak: {user.currentStreak}</p>
