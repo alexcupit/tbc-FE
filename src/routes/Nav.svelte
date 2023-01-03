d77d818640ae467ed26d4d2b226967004376a927
alexcupit <alexcupit@outlook.com>
2023-01-03 17:57:49 +0000

    how to play instructions in modal

--- src/routes/Nav.svelte
+++ src/routes/Nav.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { Link } from 'svelte-navigator';
 	let innerWidth: number;
 	import userStore from '../stores/userStore';
 	let user: object;
@@ -10,10 +9,28 @@
 
 <div class="modal" id="my-modal-2">
 	<div class="modal-box">
-		<h3 class="font-bold text-lg">Welcome to Know-It-All</h3>
-		<p class="py-4">Some text about how to play!</p>
+		<h3 class="font-bold text-lg">Welcome to Know It All</h3>
+		<section class="prose">
+			<p>Can you take on the daily quiz and prove you Know It All?</p>
+			<h4>How To Play</h4>
+			<ol>
+				<li>Clicking on PLAY will start the timer...</li>
+				<li>Choose the correct answer for each of the five questions</li>
+				<li>
+					Your score will be based on how long it takes you to complete the quiz and the number of
+					correct answers you select
+				</li>
+				<li>Every day, the quiz will refresh with five new questions!</li>
+			</ol>
+			<h4>Create an Account</h4>
+			<p>
+				Registering for an account will let you join the global leaderboard with your score from
+				each day.
+			</p>
+			<p>You can also set up your own leaderboard with friends and see how you compare each day!</p>
+		</section>
 		<div class="modal-action">
-			<a href="#" class="btn">Got it!</a>
+			<a href="/#" class="btn">Got it!</a>
 		</div>
 	</div>
 </div>
