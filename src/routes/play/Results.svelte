1cf75b4bc4dbb20eba45a53dcbcf2caf63f29970
alexcupit <alexcupit@outlook.com>
2023-01-03 18:28:56 +0000

    adds countdown to next quiz within the results component

--- src/routes/play/Results.svelte
+++ src/routes/play/Results.svelte
@@ -2,7 +2,14 @@
 	// import type stats from './+page.svelte';
 	// export let todayStats: any;
 	let todayStats = JSON.parse(localStorage.todayStats);
-	// console.log(todayStats);
+
+	let currTime = new Date().toTimeString().split(' ')[0].split(':');
+	setInterval(() => {
+		currTime = new Date().toTimeString().split(' ')[0].split(':');
+	}, 1000);
+	$: hours = 24 - +currTime[0];
+	$: mins = 60 - +currTime[1];
+	$: secs = 60 - +currTime[2];
 </script>
 
 <div class="results">
@@ -10,6 +17,29 @@
 	<h2>Today, you scored {todayStats.score}</h2>
 	<h3>you got {todayStats.correctAns} right!</h3>
 	<h3>in a total time of {todayStats.timeTaken}</h3>
+	<br />
+	<div class="grid grid-flow-col gap-5 text-center auto-cols-max">
+		<div class="flex flex-col">
+			<span class="countdown font-mono text-5xl">
+				<span style="--value:{hours}" />
+			</span>
+			hours
+		</div>
+		<div class="flex flex-col">
+			<span class="countdown font-mono text-5xl">
+				<span style="--value:{mins};" />
+			</span>
+			min
+		</div>
+		<div class="flex flex-col">
+			<span class="countdown font-mono text-5xl">
+				<span style="--value:{secs};" />
+			</span>
+			sec
+		</div>
+	</div>
+	<br />
+	<p>until the next quiz!</p>
 </div>
 
 <style></style>
