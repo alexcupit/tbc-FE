fd1c80105c35cd684393ab37106f4ffe04935596
zakaria552 <zakaria.farah55@outlook.com>
2023-01-04 16:55:07 +0000

    added badge component and axios endpoints

--- src/api.js
+++ src/api.js
@@ -8,3 +8,18 @@ export const mongoDbcreateUser = async (body) => {
     })
     return createdUser.data.user
 }
+
+export const fetchUser = async (userId) => {
+    console.log("fetching")
+    const userData = await axios.get(`https://wild-blue-rooster-belt.cyclic.app/api/users/${userId}`)
+    return userData.data.user[0]
+}
+
+export const postLeaderBoard = async (body) => {
+    const res = await axios.post(`https://wild-blue-rooster-belt.cyclic.app/api/leaderboards`, body)
+}
+
+export const patchUser = async (userId, body) => {
+    const res = await axios.patch(`https://wild-blue-rooster-belt.cyclic.app/api/users/${userId}`, body)
+    console.log(res)
+}
---
+++ src/components/Badge.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let badge;
+    export let locked;
+</script>
+    <!-- <img src={badge.src}> -->
+    <div class="img" style={`background-image: url(${badge.src})`}>
+        {#if locked}
+            <div class="overlay" style={`background-image: url("src/routes/profile/badges/padlock.png")`}></div>   
+        {/if}
+    </div>
+    <span>{badge.type}</span>
+
+<style>
+    .img {
+        width: 128px;
+        height: 130px;
+    }
+    .overlay {
+        width: 100%;
+        height: 100%;
+        filter: opacity(85%);
+    }
+    span {
+        margin: 4px;
+    }
+</style>
