5ce99700531e823a55a4820bbe49862399c4f174
dj <41585360+Deehjay@users.noreply.github.com>
2023-01-04 11:42:07 +0000

    Add animated gradient play button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,43 @@
 <h1 class="mb-5">KNOW IT ALL</h1>
-<a href="/play" class="btn btn-primary">PLAY</a>
+<a href="/play" class="btn test"><span class="text-neutral">PLAY</span></a>
+
+<style>
+	@keyframes changeBg {
+		0% {
+			background-position: 0% 50%;
+		}
+		50% {
+			background-position: 100% 50%;
+		}
+		100% {
+			background-position: 0% 50%;
+		}
+	}
+	@-webkit-keyframes changeBg {
+		0% {
+			background-position: 0% 50%;
+		}
+		50% {
+			background-position: 100% 50%;
+		}
+		100% {
+			background-position: 0% 50%;
+		}
+	}
+	@-moz-keyframes changeBg {
+		0% {
+			background-position: 0% 50%;
+		}
+		50% {
+			background-position: 100% 50%;
+		}
+		100% {
+			background-position: 0% 50%;
+		}
+	}
+	.test {
+		background: linear-gradient(270deg, rgb(229, 93, 135), rgb(95, 195, 228));
+		background-size: 200% 200%;
+		animation: changeBg 2s ease infinite;
+	}
+</style>
