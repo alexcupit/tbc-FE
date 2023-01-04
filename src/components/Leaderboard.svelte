842c3ff97008fd74811ca6c3b5513fff297af572
alexcupit <alexcupit@outlook.com>
2023-01-04 21:57:47 +0000

    leaderboard name and resolve merged comments

--- src/components/Leaderboard.svelte
+++ src/components/Leaderboard.svelte
@@ -4,6 +4,7 @@
 	let result = fetchLeaderboard(leaderboardName);
 	let position = 0;
 	let today = new Date().toISOString().split('T')[0];
+	let [year, month, day] = today.split('-');
 
 	const handleIncrementPosition = () => {
 		return (position += 1);
@@ -15,8 +16,8 @@
 </svelte:head>
 
 <article class="prose">
-	<h2 class="mb-1">Today's Leaderboard</h2>
-	<h3 class="text-center mb-1">{today}</h3>
+	<h2 class="mb-1">Today's {leaderboardName} Leaderboard</h2>
+	<h3 class="text-center mb-1">{day}-{month}-{year}</h3>
 </article>
 <div class="overflow-x-auto">
 	<table class=" table table-compact w-full text-center">
@@ -47,53 +48,5 @@
 	</table>
 </div>
 
-<!-- <div id="cont">
-	{#await result then leaderboard}
-		<p>TODAY'S {leaderboardName.toUpperCase()} LEADERBOARD</p>
-		<p id="date">{leaderboard.date.split('T')[0]}</p>
-		<div id="title">
-			<div>USERNAME</div>
-			<div>SCORE</div>
-			<div>TIME</div>
-			<div>ANSWERS</div>
-		</div>
-		{#each leaderboard.members as member}
-			<div id="cont-item">
-				<div>{member.username}</div>
-				<div>{member.todayStats.score}</div>
-				<div>{member.todayStats.timeTaken}</div>
-				<div>{member.todayStats.correctAns}</div>
-			</div>
-		{/each}
-	{/await}
-</div> -->
 <style>
-	/* p {
-		font-family: Barrio;
-		font-size: 2em;
-		margin: 5px;
-	}
-	#cont {
-		padding: 0.8em;
-		color: rgb(81, 14, 83);
-		display: flex;
-		flex-direction: column;
-		background: rgb(211, 159, 208);
-	}
-	#date {
-		align-self: center;
-	}
-	#title {
-		display: flex;
-		justify-content: space-between;
-	}
-	#cont-item {
-		padding-right: 0.8em;
-		font-family: Oswald;
-		font-size: 1.3em;
-		white-space: nowrap;
-		display: grid;
-		grid-template-columns: 25% 25% 25% 25%;
-		text-align: right;
-	} */
 </style>
