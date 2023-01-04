08de78ab8dafd347376f3248514944d0ebb73a15
dj <41585360+Deehjay@users.noreply.github.com>
2023-01-04 11:25:08 +0000

    Add styling for login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -41,87 +41,99 @@
 			required: true,
 			pattern: '[a-zA-Z0-9]{8,}'
 		}
-	]
-	let avatarStyles = ["adventurer", "adventurer-neutral", "bottts", "micah", "big-smile"]
-	let  user: object;
-	let signUp = false
-	const login = true
-	let disableButton = false
-	let firebaseError = ""
+	];
+	let avatarStyles = ['adventurer', 'adventurer-neutral', 'bottts', 'micah', 'big-smile'];
+	let user: object;
+	let signUp = false;
+	const login = true;
+	let disableButton = false;
+	let firebaseError = '';
 	userStore.subscribe(async (data) => {
-		user = data
-		if(user.isLoggedIn) await goto("/")
-	})
+		user = data;
+		if (user.isLoggedIn) await goto('/');
+	});
 	const randomStyle = (arr) => {
-		return arr[Math.floor(Math.random()*arr.length)]
-	}
+		return arr[Math.floor(Math.random() * arr.length)];
+	};
 	const onChange = (e) => {
 		values = { ...values, [e.target.placeholder]: e.target.value };
 		console.dir(e.target);
 	};
 	const submitHandleSingUp = async (e) => {
-		console.log("submitted sign up form values", values)
-		const photoURL = `https://avatars.dicebear.com/api/${randomStyle(avatarStyles)}/${values.username}.svg?size=200`
+		console.log('submitted sign up form values', values);
+		const photoURL = `https://avatars.dicebear.com/api/${randomStyle(avatarStyles)}/${
+			values.username
+		}.svg?size=200`;
 		await createUserWithEmailAndPassword(auth, values.email, values.password)
-    	.then( async (userCredential) => {
-			const userFirebase = userCredential.user
-			await updateProfile(userFirebase, {
-				displayName: values.username, photoURL})
-			console.log("created account in firebase", userFirebase)
-			const mongoDbUserBody = {username: userFirebase.displayName, userId: userFirebase.uid}
-			const createdUser = await mongoDbcreateUser(mongoDbUserBody)
-			console.log("created mongodb user", createdUser)
-			userStore.update((user) => {
-				user.isLoggedIn = true
-				user.username = createdUser.username
-				user.userId = createdUser.userId
-				user.photoURL = photoURL
-				return user
+			.then(async (userCredential) => {
+				const userFirebase = userCredential.user;
+				await updateProfile(userFirebase, {
+					displayName: values.username,
+					photoURL
+				});
+				console.log('created account in firebase', userFirebase);
+				const mongoDbUserBody = { username: userFirebase.displayName, userId: userFirebase.uid };
+				const createdUser = await mongoDbcreateUser(mongoDbUserBody);
+				console.log('created mongodb user', createdUser);
+				userStore.update((user) => {
+					user.isLoggedIn = true;
+					user.username = createdUser.username;
+					user.userId = createdUser.userId;
+					user.photoURL = photoURL;
+					return user;
+				});
+				console.log(createdUser);
+				goto('/');
+			})
+			.catch((err) => {
+				firebaseError = err.code.split('/')[1];
 			});
-			console.log(createdUser);
-			goto('/');
-		})
-		.catch((err) => {
-			firebaseError = err.code.split('/')[1];
-		});
 	};
 	const submitHandleLogin = async (e) => {
-		disableButton = true
-		console.log("submitted login form values", values)
+		disableButton = true;
+		console.log('submitted login form values', values);
 		await signInWithEmailAndPassword(auth, values.email, values.password)
-    	.then((userCredential) => {
-			const userFirebase = userCredential.user
-			userStore.update((user) => {
-				user.isLoggedIn = true
-				user.userId = userFirebase.uid
-				user.username = userFirebase.displayName
-				user.photoURL = userFirebase.photoURL
-				return user
+			.then((userCredential) => {
+				const userFirebase = userCredential.user;
+				userStore.update((user) => {
+					user.isLoggedIn = true;
+					user.userId = userFirebase.uid;
+					user.username = userFirebase.displayName;
+					user.photoURL = userFirebase.photoURL;
+					return user;
+				});
+				console.log('logged in user', user);
+				goto('/');
 			})
-			console.log('logged in user', user);
-			goto('/');
-		})
-	.catch((err) => {
-		firebaseError = err.code.split('/')[1];
-		disableButton = false;
-	});
+			.catch((err) => {
+				firebaseError = err.code.split('/')[1];
+				disableButton = false;
+			});
 	};
 </script>
 
 {#if signUp}
-	<div class="form content-center">
+	<article class="prose mb-5">
 		<h2 class="text-center">Create a new account</h2>
+	</article>
 
-		<form on:submit|preventDefault={submitHandleSingUp}>
-			{#each inputs as input}
-				<FormInput props={input} {onChange} value={values[input.label]} {firebaseError} />
-			{/each}
-			<button class="btn btn-primary">Submit</button>
-		</form>
-	</div>
+	<form
+		class="login flex-col content-center justify-center"
+		on:submit|preventDefault={submitHandleSingUp}
+	>
+		{#each inputs as input}
+			<FormInput props={input} {onChange} value={values[input.label]} {firebaseError} />
+		{/each}
+	</form>
+	<button class="btn btn-primary mb-5">Submit</button>
 {:else}
-	<h2>Login</h2>
-	<form class="login" on:submit|preventDefault={submitHandleLogin}>
+	<article class="prose mb-5">
+		<h2>Login</h2>
+	</article>
+	<form
+		class="login flex-col content-center justify-center"
+		on:submit|preventDefault={submitHandleLogin}
+	>
 		{#each inputs as input}
 			{#if input.label === 'email' || input.label === 'password'}
 				<div class="login__field">
@@ -129,13 +141,17 @@
 				</div>
 			{/if}
 		{/each}
-		<button class="btn btn-primary" disabled={disableButton}>submit</button>
 	</form>
+	<button class="btn btn-primary mb-5" disabled={disableButton}>submit</button>
 {/if}
 {#if signUp}
-	<h4 on:click={() => (signUp = false)}>I have an account</h4>
+	<article class="prose">
+		<a class="link link-accent" on:click={() => (signUp = false)}>I have an account</a>
+	</article>
 {:else}
-	<h4 on:click={() => (signUp = true)}>sign up</h4>
+	<article class="prose">
+		<a class="link link-accent" on:click={() => (signUp = true)}>Sign up</a>
+	</article>
 {/if}
 
 <style>
