762d4de4a2157673a5346cc3a1c7f8b00a747c9e
dj <41585360+Deehjay@users.noreply.github.com>
2023-01-05 10:57:06 +0000

    Refactor leaderboards get request to be async

--- src/api.js
+++ src/api.js
@@ -1,46 +1,52 @@
 import axios from 'axios';
 
 const quizAPI = axios.create({
-    baseURL: 'https://wild-blue-rooster-belt.cyclic.app/api/'
+	baseURL: 'https://wild-blue-rooster-belt.cyclic.app/api/'
 });
 
 export const mongoDbcreateUser = async (body) => {
-    console.log(body, 'injecting');
-    const createdUser = await axios
-        .post('https://wild-blue-rooster-belt.cyclic.app/api/users', body)
-        .catch((err) => {
-            console.log(err);
-        });
-    return createdUser.data.user;
+	console.log(body, 'injecting');
+	const createdUser = await axios
+		.post('https://wild-blue-rooster-belt.cyclic.app/api/users', body)
+		.catch((err) => {
+			console.log(err);
+		});
+	return createdUser.data.user;
 };
 
 export const patchUser = (userId, bodyChanges) => {
-    return quizAPI.patch(`/users/${userId}`, bodyChanges).then((res) => res.data.updatedUser);
+	return quizAPI.patch(`/users/${userId}`, bodyChanges).then((res) => res.data.updatedUser);
 };
 
 export const patchLeaderBoard = (leaderBoardName, user) => {
-    return quizAPI.patch(`/leaderboards/${leaderBoardName}`, user).then((res) => { });
+	return quizAPI.patch(`/leaderboards/${leaderBoardName}`, user).then((res) => {});
 };
 
 export const getUser = (userId) => {
-    return quizAPI.get(`/users/${userId}`).then((res) => res.data.user[0]);
+	return quizAPI.get(`/users/${userId}`).then((res) => res.data.user[0]);
 };
 
 export const fetchLeaderboard = async (leaderboardName) => {
-    return quizAPI.get(`/leaderboards/${leaderboardName}`).then(res => res.data.leaderboard);
+	const leaderboardData = await axios.get(
+		`https://wild-blue-rooster-belt.cyclic.app/api/leaderboards/${leaderboardName}`
+	);
+	return leaderboardData.data.leaderboard;
 };
 
 export const fetchUser = async (userId) => {
-    console.log("fetching")
-    const userData = await axios.get(`https://wild-blue-rooster-belt.cyclic.app/api/users/${userId}`)
-    return userData.data.user[0]
-}
+	console.log('fetching');
+	const userData = await axios.get(`https://wild-blue-rooster-belt.cyclic.app/api/users/${userId}`);
+	return userData.data.user[0];
+};
 
 export const postLeaderBoard = async (body) => {
-    const res = await axios.post(`https://wild-blue-rooster-belt.cyclic.app/api/leaderboards`, body)
-}
+	const res = await axios.post(`https://wild-blue-rooster-belt.cyclic.app/api/leaderboards`, body);
+};
 
 export const updateUser = async (userId, body) => {
-    const res = await axios.patch(`https://wild-blue-rooster-belt.cyclic.app/api/users/${userId}`, body)
-    console.log(res)
-}
+	const res = await axios.patch(
+		`https://wild-blue-rooster-belt.cyclic.app/api/users/${userId}`,
+		body
+	);
+	console.log(res);
+};
